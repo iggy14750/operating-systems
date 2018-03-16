40ac4ccd5b4d8aa2e00cb10038508e5a736e1f2f
iggy14750 <ibg4@pitt.edu>
2018-03-16 17:08:37 -0400

    Better support for generating the data I want.

--- p2-lottery/graphs.c
+++ p2-lottery/graphs.c
@@ -2,16 +2,50 @@
 #include "user.h"
 #include "pstat.h"
 
+#define P1_TICKETS 2
+#define P2_TICKETS 4
+#define P3_TICKETS 6
+#define DATA_SIZE 10
+#define STEP_SIZE 100
+
 // Spawns two child processes so that the three
 // have a 1:2:3 relationship in terms of tickets.
 int main() {
+    struct pstat ps;
+
+    printf(1, "P1\tP2\tP3\n");
+
     if (fork() == 0) {
-        settickets(2);
+        settickets(P1_TICKETS);
         while (1);
     } else if (fork() == 0) {
-        settickets(3);
+        settickets(P2_TICKETS);
+        while (1);
+    } else if (fork() == 0) {
+        settickets(P3_TICKETS);
         while (1);
     } else {
-        while (1);
+        int p1 = 0;
+        int p2 = 0;
+        int p3 = 0;
+        for (int dontuse = 0; dontuse < DATA_SIZE; dontuse++) {
+            sleep(STEP_SIZE);
+            getpinfo(&ps);
+            for (int j = 0; j < NPROC; j++) {
+                switch (ps.tickets[j]) {
+                    case P1_TICKETS:
+                        p1 = ps.ticks[j];
+                        break;
+                    case P2_TICKETS:
+                        p2 = ps.ticks[j];
+                        break;
+                    case P3_TICKETS:
+                        p3 = ps.ticks[j];
+                        break;
+                }
+            }
+            printf(1, "%d\t%d\t%d\n", p1, p2, p3);
+        }
+        exit();
     }
 }
