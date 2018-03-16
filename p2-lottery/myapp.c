578a113e463a464da5d060eabb5ce91818aad15b
iggy14750 <ibg4@pitt.edu>
2018-03-16 15:06:57 -0400

    Only displays 'inuse' entries in the pstat

--- p2-lottery/myapp.c
+++ p2-lottery/myapp.c
@@ -7,10 +7,10 @@ int main() {
   struct pstat ps;
   settickets(2);
   getpinfo(&ps);
-  printf(1, "Index\tInuse\tTickets\tPID\tTicks\n");
+  printf(1, "Index\tTickets\tPID\tTicks\n");
   for (int i = 0; i < NPROC; i++) {
-    printf(1, "%d\t%d\t%d\t%d\t%d\n", i, 
-      ps.inuse[i],
+    if (ps.inuse[i] == 0) continue;
+    printf(1, "%d\t%d\t%d\t%d\n", i, 
       ps.tickets[i],
       ps.pid[i],
       ps.ticks[i]
