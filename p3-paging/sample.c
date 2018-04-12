63fda4e620623500fed9e6054ccbecc4dba1c71a
Max Reno <mar257@pitt.edu>
2018-04-12 12:11:37 -0400

    Simple program - allocates and assigns some ints to heap

--- p3-paging/sample.c
+++ p3-paging/sample.c
@@ -5,11 +5,16 @@
 int main()
 {
   printf(1, "Hellooo\n");
-  int * mem = (int *) sbrk(4096);
+  char *b, *oldbrk;
+  oldbrk = sbrk(0);
+  // printf(1, "oldbrk - %x\n", oldbrk);
   int i;
-  for(i = 0; i < 4095; i++){
-    *mem = 'a';
+  for(i = 0; i < 100; i++){
+    b = sbrk(1);
+    *b = i;
+    // printf(1, "i - %d\tb - %x\n", i, b);
   }
+  // printf(1, "b - %d\n", *b);
 
   exit();
 }
