9e959f5e8ffbf577066e838a6e554f7a82f0a0e1
Max Reno <mar257@pitt.edu>
2018-04-11 23:06:57 -0400

    I might be learning

--- p3-paging/sample.c
+++ p3-paging/sample.c
@@ -5,10 +5,10 @@
 int main()
 {
   printf(1, "Hellooo\n");
-  void* mem = sbrk(4096);
+  int * mem = (int *) sbrk(4096);
   int i;
   for(i = 0; i < 4095; i++){
-    mem = 'a';
+    *mem = 'a';
   }
 
   exit();
