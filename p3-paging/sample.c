df186d4db9a384f98d24e08ec1da8cb5aadfb321
Max Reno <mar257@pitt.edu>
2018-04-11 21:24:56 -0400

    Change kalloc to sbrk

--- p3-paging/sample.c
+++ p3-paging/sample.c
@@ -1,12 +1,11 @@
 #include "types.h"
 #include "user.h"
 #include "syscall.h"
-#include "kalloc.c"
 
 int main()
 {
   printf(1, "Hellooo\n");
-  char *buf = kalloc();
+  char *buf = sbrk();
   int i;
   for(i = 0; i < 4095; i++){
     *buf = 'a';
