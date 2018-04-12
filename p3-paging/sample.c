b251efc92f88c1e2e76df83e85d37d11f349dca6
Max Reno <mar257@pitt.edu>
2018-04-12 12:57:01 -0400

    Does some more stuff

--- p3-paging/sample.c
+++ p3-paging/sample.c
@@ -4,17 +4,39 @@
 
 int main()
 {
-  printf(1, "Hellooo\n");
-  char *b, *oldbrk;
+  printf(1, "Sample Test\n");
+  int pid;
+  char *a, *b, *c, *oldbrk;
+
   oldbrk = sbrk(0);
-  // printf(1, "oldbrk - %x\n", oldbrk);
+  printf(1, "oldbrk - %x\n", oldbrk);
+
   int i;
-  for(i = 0; i < 100; i++){
+  for(i = 0; i < 4096; i++){
     b = sbrk(1);
     *b = i;
     // printf(1, "i - %d\tb - %x\n", i, b);
+    // printf(1, "b - %d\n", *b);
   }
-  // printf(1, "b - %d\n", *b);
+
+  // Deallocate
+  a = sbrk(0);
+  c = sbrk(-4096);
+  if(c == (char*)0xffffffff){
+    printf(1, "Did not deallocate\n");
+    exit();
+  }
+  c = sbrk(0);
+  if(c != a - 4096){
+    printf(1, "Sbrk deallocation produced wrong address, a %x c %x\n", a, c);
+    exit();
+  }
+
+  // Fork Test
+  pid = fork();
+  if(pid < 0){
+    exit();
+  }
 
   exit();
 }
