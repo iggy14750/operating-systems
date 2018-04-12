059f5550e680b45088e8644a503da70c9620dd47
Max Reno <mar257@pitt.edu>
2018-04-11 21:28:47 -0400

    I'm really bad at c

--- p3-paging/sample.c
+++ p3-paging/sample.c
@@ -5,10 +5,10 @@
 int main()
 {
   printf(1, "Hellooo\n");
-  char *buf = sbrk();
+  int *buf = sbrk();
   int i;
   for(i = 0; i < 4095; i++){
-    *buf = 'a';
+    *buf = (int) 'a';
   }
 
   exit();
